7de3f4713b8dd5ccc6fd9fdf26147e37e2adf606
pierina <pierina@beecoded.ro>
2023-05-19 14:59:36 -0300

    Added routes and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from "react";
-import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+import Home from "./pages/home/home";
+import Onboarding from "./pages/onboarding/onboarding";
+
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Navigate to="/home" />} />
+
+      <Route path="/home" element={<Home />} />
+      <Route path="/onboarding" element={<Onboarding />} />
+    </Routes>
   );
 }
 
