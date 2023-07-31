c0959cadbd5626f4e98ea0056be7551fe356b128
Pierina <pierina@beecoded.ro>
2023-07-31 14:29:14 -0300

    modal

--- src/components/card-modal/card-modal.tsx
+++ src/components/card-modal/card-modal.tsx
@@ -15,9 +15,6 @@ const CardModal = ({ title, image, children, isOpen, onClose }: ModalProps) => {
   return (
     <div className={styles.modalOverlay} style={{ display: isOpen ? "block" : "none" }}>
       <div className={styles.modalContent}>
-        <span className={styles.closeButton} onClick={onClose}>
-          &times;
-        </span>
         <h3>{title}</h3>
         <div>
           {children}
